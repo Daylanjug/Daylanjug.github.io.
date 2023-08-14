bfe77e47297e5862763785809918c6e3c6c5485c
Daylanjug <142190973+Daylanjug@users.noreply.github.com>
2023-08-13 21:21:06 -0600

    Mi primera pagina wed JA

---
+++ style.css
@@ -0,0 +1,93 @@
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,600&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+header{ 
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: rgba(219, 187, 150, 0.904);
+    width: 100%;
+    height: 100%;
+    
+}
+header ul{ 
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+}
+header ul li{ 
+    margin: 12px;
+}
+img{ 
+    width: 100px;
+}
+h1{
+    font-size: 26px;
+    color: rgba(161, 12, 12, 0.705);
+    font-family: 'Playfair Display', serif;
+ }
+h3{ 
+    font-size: 21px;
+    color: rgb(94, 34, 6);
+    font-family: 'Playfair Display', serif;
+}
+p{ 
+    color: rgb(11, 11, 12);
+    font-family: 'Playfair Display', serif;
+}
+.contenedor{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    margin: 10px 0 10px 0;
+    background-color: rgba(138, 110, 73, 0.767);
+ }
+ .cj{ 
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+ }
+ .cj img{ 
+    border-radius: 50%;
+ }
+ .cj p{ 
+    font-size: 15px;
+ }
+ .present{ 
+    width: 79%;
+    font-size: 20px;
+    padding: 93px;
+    background-color: antiquewhite;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 10px 0 10px 0;
+ }
+ #Val{ 
+    width: 79%;
+    height: 150px;
+    font-size: 20px;
+    padding: 93px;
+    background-color: rgb(207, 194, 181);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 10px 0 10px 0;
+ }
+ #nosotros{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+ }
+ #nosotros iframe{ 
+    padding: 10px;
+     margin: 10px;
+ }
+ 
